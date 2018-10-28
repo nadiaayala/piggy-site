fe15a5754046f49a3aeaa53915b698c2aa23f758
Nadia Ayala <nadiaayalanogueira@outlook.com>
2018-10-28 15:08:58 +0400

    email validation

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,34 @@ $(function(){
     const $btnSubscriber = $('#btnSubscriber');
     const $form = $('#name');
     const $modal = $('#myModal');
+    var $alertDiv
 
     $btnSubscriber.on('click', function(e){
         e.preventDefault();        
-        if(validate(5)){
+        if(validate(5) && validateEmail()){
             $btnSubscriber.attr("data-toggle", "modal");
             $btnSubscriber.attr("data-target", "#myModal");            
-    }});
+    }
+    });  
+
     function validate(minLength){
         if ($nameSubscriber.val().length<minLength){
-        var $alertDiv = $('<div class="alert alert-danger alert-dismissible"> <p> O nome precisa ter mais que 5 caracteres!</p> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>');
+            $alertDiv = $('<div class="alert alert-danger alert-dismissible"> <p> O nome precisa ter mais que 5 caracteres!</p> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>');
             $form.prepend($alertDiv);   
-        return false;
-    }
-        else{
+            return false;
+        }
+        if(validateEmail()){;
+            return true;
+        }};
+
+   function validateEmail(){
+        var $emailText = $emailSubscriber.val();
+        if($emailText.indexOf('@') != -1){
             return true;
         }
-    }
-   
-
+        $alertDiv = $('<div class="alert alert-danger alert-dismissible"> <p> Email inválido! </p> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>');
+        $form.prepend($alertDiv);
+        return false;
+            
+}
 });
